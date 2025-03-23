86fbb733fb0ddaca7642b38a7917e61af124f716
许圣峰 <kricsleo@163.com>
2025-03-23 20:06:35 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function grab() {
-  console.log('working...');
+import fs from 'node:fs/promises'
+import { load } from 'cheerio'
+
+const URL = 'https://www.totaltypescript.com/tsconfig-cheat-sheet'
+
+sync()
+
+async function sync() {
+  const tsConfig = await grab()
+  await fs.writeFile('tsconfig.cheat-sheet.json', tsConfig)
+}
+
+export async function grab() {
+  const html = await (await fetch(URL)).text()
+  const $ = load(html)
+  const tsConfig = $($('pre > code')[0]!).text()
+  return tsConfig
 }
